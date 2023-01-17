905c90891b3043981f2e7b8ecede58402b025341
TalDahan2304 <tald234097@gmail.com>
2023-01-18 01:33:15 +0200

    legend component

--- webassignment/src/App.js
+++ webassignment/src/App.js
@@ -1,15 +1,24 @@
 import './App.css'; 
-
 import data from './details.json'
-import React, { Component } from "react";
+import React from "react";
+import Legend from './components/Legend/Legend'
 
 function App() {
-  return (
-    <div className="App">
+  // console.log(data.Legends[1]);
 
-        {data.MonitorType.map((item) => (
-          <button id="monitortypebtns">{item.Name}</button>                
-        ))}
+  return (
+
+    <div className="App">
+        {/* {data.MonitorType.map((item,i) => (
+          <button className="monitortypebtns" key={i} id={item.Id}  onClick = {()=>setActive(console.log(item.Name, item.Id))} >{item.Name} </button> 
+        ))} */}
+          
+        <div style={{
+          height:"100vh",
+          width:"100vw",
+        }}>
+        <Legend tags={data.Legends[1].tags}/>
+        </div>
     </div>
   );
 }
---
+++ webassignment/src/components/Legend/Legend.js
@@ -0,0 +1,39 @@
+import './LegendStyle.css'
+import React  from "react";
+
+const LegendItem = (props) =>{
+    const {label, color} = props;
+    return(
+        <div className="legend-wrapper">
+            <div className = "color" style={{
+                height: 15,
+                width: 15,
+                backgroundColor: `${color}`
+            }}></div>
+            <div>
+                {label}
+            </div>
+        </div>
+    )
+    
+}
+
+
+const Legend = (props) =>{
+    const {tags}= props;
+    return(
+        <div style={{
+            height:"50vh",
+            width:"50vw",
+          }}>
+       
+        {tags?.map(item => {
+            return <LegendItem color={item.Color} label={item.Label}/>
+        })}
+        
+       </div>
+    )
+   
+}
+
+export default Legend;
